bd25db3f5a110692a84e3272585f14b210246e7b
abhishek <abhishekb740@gmail.com>
2025-07-17 20:22:38 +0000

    feat: basic backend repo setup completed

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
+
+
